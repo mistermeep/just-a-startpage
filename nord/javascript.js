bf9d2d6603e803c8feb548e24e4d86fb42071581
mistermeep <80411534+mistermeep@users.noreply.github.com>
2021-04-17 17:47:41 -0400

    Update javascript.js

--- nord/javascript.js
+++ nord/javascript.js
@@ -36,6 +36,41 @@ function setFocus(){
 	document.getElementById("q").focus();
 }
 
+$(document).ready(function() {
+      $(document).bind('keydown', function(e) {
+        if (e.which == 65) {
+          window.location.href = "https://archiveofourown.org/";
+        }
+        if (e.which == 68) {
+          window.location.href = "https://docstransfer.com/";
+		  }
+        if (e.which == 66) {
+          window.location.href = "https://brainpop.com/";
+		  }
+        if (e.which == 71) {
+          window.location.href = "https://github.com/";
+		  }
+        if (e.which == 67) {
+          window.location.href = "https://crunchyroll.com/";
+		  }
+        if (e.which == 83) {
+          window.location.href = "https://benhill.schoology.com/";
+		  }
+        if (e.which == 70) {
+          window.location.href = "https://office.com/";
+		  }
+        if (e.which == 82) {
+          window.location.href = "https://reddit.com/";
+		  }
+        if (e.which == 87) {
+          window.location.href = "https://twitter.com/";
+		  }
+        if (e.which == 86) {
+          window.location.href = "https://tumblr.com/";
+        }
+      });
+});
+
 
 
 window.addEventListener('load', () => {
