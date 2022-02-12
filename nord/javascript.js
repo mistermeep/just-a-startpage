d09c00ae5443a65cd2e99eaa8876d1abf64498da
mistermeep <80411534+mistermeep@users.noreply.github.com>
2022-02-11 21:21:19 -0500

    Update javascript.js

--- nord/javascript.js
+++ nord/javascript.js
@@ -1,9 +1,42 @@
-const determineGreet = hours => document.getElementById("greeting").innerText = `good ${hours < 12 ? "morning!" : hours < 18 ? "afternoon!" : "evening!"}`
+const username = "meep";
+const determineGreet = hours => document.getElementById("greeting").innerText = `good ${hours < 12 ? "morning!" : hours < 18 ? "afternoon!" : "evening!"}`;
+
+var m_names = ["january", "february", "march", 
+"april", "may", "june", "july", "august", "september", 
+"october", "november", "december"];
+
+var d_names = ["sunday","monday", "tuesday", "wednesday", 
+"thursday", "friday", "saturday"];
+
+var myDate = new Date();
+myDate.setDate(myDate.getDate());
+var curr_date = myDate.getDate();
+var curr_month = myDate.getMonth();
+var curr_day  = myDate.getDay();
+
+    var now = new Date();
+    var TwentyFourHour = now.getHours();
+    var hour = now.getHours();
+    var min = now.getMinutes();
+    var mid = 'pm';
+    if (min < 10) {
+      min = "0" + min;
     }
+    if (hour > 12) {
+      hour = hour - 12;
+    }    
+    if(hour==0){ 
+      hour=12;
+    }
+    if(TwentyFourHour < 12) {
+       mid = 'am';
+    }     
 function setFocus(){
 	document.getElementById("q").focus();
 }
 });
+
+
 
 window.addEventListener('load', () => {
 	setFocus();
