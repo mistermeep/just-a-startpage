09cfe674ad582aee0667e750cefeb621d3834f34
mistermeep <80411534+mistermeep@users.noreply.github.com>
2022-02-11 21:13:20 -0500

    Update javascript.js

--- nord/javascript.js
+++ nord/javascript.js
@@ -1,14 +1,13 @@
-
-const username = "meep";
 const determineGreet = hours => document.getElementById("greeting").innerText = `good ${hours < 12 ? "morning!" : hours < 18 ? "afternoon!" : "evening!"}`
+    }
 function setFocus(){
 	document.getElementById("q").focus();
 }
 });
 
-
-
 window.addEventListener('load', () => {
 	setFocus();
+    let today = new Date();
+    determineGreet(new Date().getHours());
     setTimeout(() => { document.getElementById("greeting").style.opacity = "1"; }, 100);
 })
